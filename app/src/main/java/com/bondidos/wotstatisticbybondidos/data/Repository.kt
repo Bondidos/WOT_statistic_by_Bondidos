9468f013b88e6c0229eaf104af33a8fa0b132003
Bondidos <gglbond@gmail.com>
2021-12-09 21:36:59 +0300

    ui debugging

--- app/src/main/java/com/bondidos/wotstatisticbybondidos/data/Repository.kt
+++ app/src/main/java/com/bondidos/wotstatisticbybondidos/data/Repository.kt
@@ -1,6 +1,5 @@
 package com.bondidos.wotstatisticbybondidos.data
 
-import android.util.Log
 import com.bondidos.wotstatisticbybondidos.data.sources.api.WotApi
 import com.bondidos.wotstatisticbybondidos.data.sources.room.RoomRepositoryDao
 import com.bondidos.wotstatisticbybondidos.data.sources.sharedPrefs.PrefStoreImpl
@@ -12,8 +11,6 @@ import com.bondidos.wotstatisticbybondidos.domain.constatnts.Constants.FIELDS_AC
 import com.bondidos.wotstatisticbybondidos.domain.constatnts.Constants.FIELDS_DATA
 import com.bondidos.wotstatisticbybondidos.domain.entityes.MultiViewModel
 import com.bondidos.wotstatisticbybondidos.domain.entityes.User
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
@@ -23,16 +20,13 @@ class RepositoryImpl @Inject constructor(
     private val utils: Utils
 ) : Repository {
 
-    //todo delete dispatcher. room and retrofit has yours IO functionality
     override suspend fun createAchievesDB() {
-        withContext(Dispatchers.IO) {
             val list = utils.jsonToAchievesList()
             roomStorage.createAchievesDB(list)
-        }
     }
 
     override suspend fun isAchievesDataBaseExist(): Boolean =
-        withContext(Dispatchers.IO) { roomStorage.isAchievesDBExist() == ACHIEVES_COUNT }
+        roomStorage.isAchievesDBExist() == ACHIEVES_COUNT
 
     override suspend fun saveUser(url: String): Boolean {
 
@@ -62,10 +56,6 @@ class RepositoryImpl @Inject constructor(
             apiData.data["${user.account_id}"]?.clanId ?: 0
         )
 
-        Log.d("Repository", user.toString())
-        /*Log.d("Repository", apiClan.toString())
-        Log.d("Repository", apiData.data.toString())*/
-
         return utils.createMultiViewModelList(
                 apiData,
                 apiClan,
@@ -91,16 +81,6 @@ class RepositoryImpl @Inject constructor(
             achievesByNameFromDB,
             apiAchievesResponse.data["${user.account_id}"]?.achievements ?: emptyMap()
         )
-
-        //Log.d("Repository",result.size.toString())
-        //Log.d("Repository",result.toString())
-        //Log.d("Repository",achievesByNameFromDB.size.toString())
-        //Log.d("Repository",achievesNamesList.toString())
-        /*Log.d("Repository","achievesNamesList.size = ${achievesNamesList.size}," +
-                "apiAchievesResponse.data.size= " +
-                "${apiAchievesResponse.data["${user.account_id}"]?.achievements?.size}")*/
-        //Log.d("Repository",apiAchievesResponse.toString())
-
     }
 
     override suspend fun logout(): Boolean {
@@ -109,10 +89,6 @@ class RepositoryImpl @Inject constructor(
         prefStore.logout()
         return true
     }
-//nickname=LegitimateKiller,
-// account_id=560508396,
-// access_token=9172617952f3348333b2cef4641a923e6b74ab42,
-// expires_at=1639935775
 }
 
 
